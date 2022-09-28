6a823a5f99db8e4281a1a52e4ad826c948f4db74
JakobAdamsson <jakobmeadamsson@gmail.com>
2022-09-28 09:57:15 +0200

    Final version

--- task3.py
+++ task3.py
@@ -1,4 +1,6 @@
-from turtle import update
+"""GA Written by Jakob Adamsson"""
+
+
 import numpy as np
 import math
 import random
@@ -7,20 +9,15 @@ import names
 from typing import Tuple, List
 from finalMaze import finalMaze
 from zeroMaze import zeroMaze
-# 14,6,1,9
-# 20, 1, 20, 33
 
 "MAZE CONFIG"
 ORIGIN = (20, 1)
 GOAL = (20, 33)
 MAZE = finalMaze(20, 1, 20, 33)
-#ORIGIN = (14, 6)
-#GOAL = (1,9)
-#MAZE = zeroMaze(14,6,1,9)
 
 """MICE CONFIG"""
 POPULATION_SIZE = 100
-N_STEPS = 1750
+N_STEPS = 1500
 MUTATION_CHANCE = 0.4
 NUM_MUTATIONS = int(N_STEPS*0.65)
 EPOCHS = 1000
@@ -33,13 +30,12 @@ class Moves():
     RIGHT = 4
     
 class Mouse():
-    def __init__(self, name,n_steps, fitness = 0, current_pos=None, start_pos = ORIGIN, found_goal = False):
+    def __init__(self, name, n_steps, fitness = 0, current_pos=None, start_pos = ORIGIN):
         self._name = name
         self._n_steps = n_steps
         self._fitness = fitness
         self._current_pos = current_pos
         self._start_pos = start_pos
-        self._found_goal = found_goal
         self._path = MAZE.copy()
         self._trail = []
         self._camper = 0
@@ -50,61 +46,49 @@ class Mouse():
     def camper(self) -> int:
         return self._camper
     
-    def inc_camper(self):
-        self._camper += 1
-    
     @property
     def trail(self) -> List[Tuple[int, int]]:
         return self._trail 
-       
-    def add_to_trail(self, score: int):
-        self._trail.append(score)
         
     @property
     def path(self) -> List[Tuple[int, int]]:
         return self._path
 
-    def update_path(self, path_step: Tuple[int, int]):
-        self._path[path_step[0]][path_step[1]] = 6
-    
-    @property
-    def goal(self) -> bool:
-        return self._found_goal
-    
-    def found_goal(self):
-        self._updated = True
+    @path.setter
+    def path(self, path_step: Tuple[int, int]):
+        x, y = path_step
+        self._path[x][y] = 6
     
     @property
     def name(self) -> str:
         return self._name
-
-    @property
-    def score(self) -> int:
-        return self._score
-
-    def set_new_score(self, new_score: int):
-        self._score = new_score
     
     @property
     def current_pos(self) -> Tuple[int, int]:
         return self._current_pos
     
-    def update_current_pos(self, new_pos: Tuple[int, int]):
+    @current_pos.setter
+    def current_pos(self, new_pos: Tuple[int, int]):
         self._current_pos = new_pos
     
     @property
     def moves(self) -> List[int]:
         return self._moves
     
-    def set_new_moves(self, new_moves: List[int]):
+    @moves.setter
+    def moves(self, new_moves: List[int]):
         self._moves = new_moves
-    
-    def move(self, index: int) -> int:
-        return self._moves[index]
-    
+
     @property
     def fitness(self) -> int:
         return self._fitness
+    
+
+    def inc_camper(self):
+        self._camper += 1
+    
+    def move(self, index: int) -> int:
+        return self._moves[index]
 
     def camper_fitness(self):
         self._fitness = 1000
@@ -113,6 +97,7 @@ class Mouse():
         self._fitness = 0
 
     def set_new_fitness(self, goal: Tuple[int, int]):
+        """Fitness calculated with the help of the euclidan distance, math formula: d = √[ (x_2 - x_1)^2 + (y_2 - y_1)^2]"""
         if self.camper > 25:
             self.camper_fitness()
             return
@@ -151,8 +136,8 @@ def walk_maze(mice: List[Mouse], maze: MAZE, n_steps: int):
         for mouse in mice:
             new_move = mouse.get_new_move(step)
             if check_if_valid_move(maze, new_move): 
-                mouse.update_current_pos(new_move)
-                mouse.update_path(new_move)
+                mouse.current_pos = new_move
+                mouse.path = new_move
                 if mouse.current_pos == GOAL:
                     return
                 update_maze(maze, new_move)
@@ -175,14 +160,13 @@ def crossover(mice: List[Mouse], n_steps: int, population_size: int, mutation_ch
         cut_index = random.randint(0, n_steps)
         new_moves = dad_mouse.moves[:cut_index] + mom_mouse.moves[cut_index:]
         baby_mouse = Mouse(name = names.get_full_name(),n_steps=n_steps,current_pos = origin, start_pos=origin)
-        baby_mouse.set_new_moves(new_moves)
+        baby_mouse.moves = new_moves
      
         if random.uniform(0, 1) < mutation_chance:
             mutate(baby_mouse, num_mutations)
             
         new_generation.append(baby_mouse)
 
-    #lägga till så att för varje epoch går de extra steg, detta måste göras parallellt med att mössen får nya moves varje epok
     return top_mice + new_generation
 
 def fill_maze(mouse: Mouse):
@@ -208,7 +192,7 @@ def main():
             break
         
         
-        
+        population = crossover(population, N_STEPS, POPULATION_SIZE, MUTATION_CHANCE, ORIGIN, NUM_MUTATIONS)
         if epoch % 10 == 0:
             print(f"\nbest mice of generation {epoch}:\n{population[0].name} {population[0].fitness} {population[0].current_pos} {population[0].camper}")
             for row in population[0].path:
@@ -219,11 +203,6 @@ def main():
                 thing = [elem if elem != '9' else '@' for elem in thing]
                 print(' '.join(thing))  
             print(f"Current config:\ncurrent population size: {POPULATION_SIZE}, current steps: {N_STEPS}, current mutations {NUM_MUTATIONS}, current mutation chance {MUTATION_CHANCE}")
-            #time.sleep(5)
-        
-        
-        population = crossover(population, N_STEPS, POPULATION_SIZE, MUTATION_CHANCE, ORIGIN, NUM_MUTATIONS)
-        
     
     
     print(f"\nAnd the winner is.... \n{winner_winner_chicken_dinner[0].name}\n{winner_winner_chicken_dinner[0].fitness}\n{winner_winner_chicken_dinner[0].current_pos}")
